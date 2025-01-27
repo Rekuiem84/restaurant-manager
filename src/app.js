fd4f65ae348688705a8537e22221a86b743d680a
Rekuiem84 <morinijoshua@gmail.com>
2025-01-27 09:56:46 +0100

    setup app

--- src/app.js
+++ src/app.js
@@ -1,7 +1,18 @@
 const express = require("express");
 const mongoose = require("mongoose");
 
+const commandeRoutes = require("./routes/commande");
+const ingredientRoutes = require("./routes/ingredient");
+const platRoutes = require("./routes/plat");
+const produitRoutes = require("./routes/produit");
+const recetteRoutes = require("./routes/recette");
+const reservationRoutes = require("./routes/reservation");
+const roleRoutes = require("./routes/role");
+const salleRoutes = require("./routes/salle");
+const userRoutes = require("./routes/user");
+
 const app = express();
+app.use(express.json());
 
 app.use((req, res, next) => {
 	res.setHeader("Access-Control-Allow-Origin", "*"); // Permet de dire que tout le monde peut y accéder
@@ -18,7 +29,8 @@ app.use((req, res, next) => {
 
 mongoose
 	.connect(
-		"mongodb+srv://lucaBogoss:azerty@cluster0.wdfge.mongodb.net/casaProject?retryWrites=true&w=majority",
+		// "mongodb+srv://joshAdmin:KSdLKW83iM6uM5z@cluster0.wdfge.mongodb.net/restaurant?retryWrites=true&w=majority",
+		"mongodb+srv://joshAdmin:KSdLKW83iM6uM5z@cluster0.wdfge.mongodb.net/restaurant",
 		{
 			useNewUrlParser: true,
 			useUnifiedTopology: true,
@@ -26,5 +38,17 @@ mongoose
 	)
 	.then(() => console.log("Connexion à MongoDB réussie !"))
 	.catch(() => console.log("Connexion à MongoDB échouée !"));
+
+app.use("/api/commandes", commandeRoutes);
+app.use("/api/ingredients", ingredientRoutes);
+app.use("/api/plats", platRoutes);
+app.use("/api/produits", produitRoutes);
+app.use("/api/recettes", recetteRoutes);
+app.use("/api/reservations", reservationRoutes);
+app.use("/api/roles", roleRoutes);
+app.use("/api/salles", salleRoutes);
+app.use("/api/users", userRoutes);
+
+app.use(express.static("public"));
 
 module.exports = app;
